5ab0b9de2ccb7f8e337f8e80db53a3f0d6b6dd1b
gabriel <gabrielcmoris@gmail.com>
2024-06-29 16:23:42 +0200

    first challenge

---
+++ create-a-movie-rating-app-1/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive, ref } from "vue";
+import { items } from "./movies.json";
+import { StarIcon } from "@heroicons/vue/24/solid";
+
+const movies = reactive(items);
+console.log(movies);
+</script>
+
+<template>
+  <section class="flex flex-row flex-wrap gap-5 items-center p-5 justify-center">
+    <div class="flex flex-col items-start justify-between rounded overflow-hidden bg-white pb-5" v-for="movie in movies" :key="movie.id">
+      <img class="w-64 max-h-80 object-cover" :src="movie.image" />
+      <section class="px-5">
+        <p class="font-bold">{{ movie.name }}</p>
+        <div class="flex flex-row gap-1">
+          <p class="text-xs bg-blue-500 rounded-full px-2 py-0.5 text-white" v-for="genre in movie.genres">{{ genre }}</p>
+        </div>
+        <p class="max-w-52 text-xs text-slate-800 mt-4">
+          {{ movie.description }}
+        </p>
+        <p class="text-sm mt-4 flex items-center gap-2 flex-row">
+          Rating ({{ movie.rating }}/5)
+          <span class="flex flex-row items-center"><StarIcon v-for="n in movie.rating" class="text-yellow-500 w-4" /></span>
+        </p>
+      </section>
+    </div>
+  </section>
+</template>
