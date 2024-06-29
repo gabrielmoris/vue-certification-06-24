2a8eb9dba8349bc2129181ce0b18904ae82d7ec5
gabriel <gabrielcmoris@gmail.com>
2024-06-29 17:12:32 +0200

    add stars edition and uix of rating

--- create-a-movie-rating-app-1/src/App.vue
+++ create-a-movie-rating-app-1/src/App.vue
@@ -1,18 +1,30 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { ref } from "vue";
 import { items } from "./movies.json";
 import { StarIcon } from "@heroicons/vue/24/solid";
 
-const movies = reactive(items);
-console.log(movies);
+const movies = ref(items);
+
+const handleStar = (movieID, n) => {
+  movies.value[movieID - 1].rating = n;
+};
 </script>
 
 <template>
-  <section class="flex flex-row flex-wrap gap-5 items-center p-5 justify-center">
-    <div class="flex flex-col items-start justify-between rounded overflow-hidden bg-white pb-5" v-for="movie in movies" :key="movie.id">
+  <section class="flex flex-row flex-wrap gap-5 items-center justify-center">
+    <div
+      class="relative flex h-[32rem] flex-col items-start justify-between rounded overflow-hidden bg-white"
+      v-for="movie in movies"
+      :key="movie.id"
+    >
+      <aside class="absolute top-2 right-2">
+        <StarIcon class="w-9 text-yellow-500" :class="{ 'text-slate-500': !movie.rating }" />
+        <span class="font-sm absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">{{ movie.rating ? movie.rating : "-" }}</span>
+      </aside>
+
       <img class="w-64 max-h-80 object-cover" :src="movie.image" />
-      <section class="px-5">
-        <p class="font-bold">{{ movie.name }}</p>
+      <section class="px-5 flex flex-col justify-between pb-2 h-full w-full">
+        <p class="font-bold pt-2">{{ movie.name }}</p>
         <div class="flex flex-row gap-1">
           <p class="text-xs bg-blue-500 rounded-full px-2 py-0.5 text-white" v-for="genre in movie.genres">{{ genre }}</p>
         </div>
@@ -21,7 +33,11 @@ console.log(movies);
         </p>
         <p class="text-sm mt-4 flex items-center gap-2 flex-row">
           Rating ({{ movie.rating }}/5)
-          <span class="flex flex-row items-center"><StarIcon v-for="n in movie.rating" class="text-yellow-500 w-4" /></span>
+          <span class="flex flex-row items-center">
+            <button v-for="n in 5" @click="() => handleStar(movie.id, n)" :disabled="n === movie.rating">
+              <StarIcon class="w-4" :class="n <= movie.rating ? 'text-yellow-500' : 'text-slate-500'" />
+            </button>
+          </span>
         </p>
       </section>
     </div>
